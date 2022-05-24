5d9a71d560a925c905746333036bcbe1033a6e54
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-05-24 16:31:14 -0300

    Exercicio de escrever a primeira funcao

---
+++ primeira_funcao.js
@@ -0,0 +1,8 @@
+// //use a palavra-chave functionpara definir sua função, o nome da função deve ser helloWorld(não se esqueça do () and {})
+// use a palavra-chave var(ou letou const) para definir uma variável str, o valor de strdeve ser uma string: "Hello World!"(não esqueça do =).
+// digite o console.log()na próxima linha (não se esqueça de colocar o strentre colchetes).
+
+function helloWorld(){
+    var str = "Hello World!"
+    console.log(str)
+  }
